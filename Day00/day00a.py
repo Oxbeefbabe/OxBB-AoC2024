b3c98ec8b89da8856815e522e2c2e4e52f646661
LNemiro <ludwignemiro@yahoo.com>
2024-12-07 23:01:17 +0000

    Template to quickly start new days

---
+++ Day00/day00a.py
@@ -0,0 +1,14 @@
+EXAMPLE = True
+input_path = "example" if EXAMPLE else "input"
+
+
+def get_input(path):
+    input_text = ""
+    with open(path, "r") as file:
+        for line in file:
+            input_text += line
+    return input_text
+
+
+if __name__ == "__main__":
+    print(get_input(f"src/inputA/{input_path}.txt"))
